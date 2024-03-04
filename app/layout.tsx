af2a1e8ab2996b92d525320d17ea3ddb19f583a0
SmashUncleSmithy <56916928+cjdcjd10021@users.noreply.github.com>
2024-03-04 15:48:09 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
         {/* Google AdSense script */}
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8339895143846620" crossorigin="anonymous"></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8339895143846620" crossOrigin="anonymous"></script>
       </head>
       <body>
         {children}
