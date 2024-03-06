8e3d208ef42e4b6140cb5112ad709026ba7f4880
SmashUncleSmithy <56916928+cjdcjd10021@users.noreply.github.com>
2024-03-06 17:02:21 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,15 @@ export default function RootLayout({
         <script src="/serviceWorkerRegister.js" defer></script>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8339895143846620"
      crossOrigin="anonymous"></script>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-EQ27PZH0VM"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-EQ27PZH0VM');
+        </script>
       </head>
       <body>
         {children}
