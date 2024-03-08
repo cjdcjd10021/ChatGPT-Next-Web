4eb431300e8a5728cae36426027321124a933d7a
SmashUncleSmithy <56916928+cjdcjd10021@users.noreply.github.com>
2024-03-08 15:40:11 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,1 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8339895143846620"
+            crossOrigin="anonymous"
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
