09ac0386ee6aa46e9fd5cf9a251002ce8519ac4a
SmashUncleSmithy <56916928+cjdcjd10021@users.noreply.github.com>
2024-03-08 16:13:56 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,11 +5,28 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          {/* Google AdSense */}
           <script
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8339895143846620"
             crossOrigin="anonymous"
           ></script>
+
+          {/* Google Analytics */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-EQ27PZH0VM"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-EQ27PZH0VM');
+              `,
+            }}
+          />
         </Head>
         <body>
           <Main />
