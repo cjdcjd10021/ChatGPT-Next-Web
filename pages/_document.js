530da60330aa13c82651879382579ab7006fe39f
SmashUncleSmithy <56916928+cjdcjd10021@users.noreply.github.com>
2024-03-18 16:32:20 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,38 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script'; // 引入 Script 组件
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          {/* 静态文件，如样式表或`favicon`等，可以在这里添加 */}
+          {/* Google AdSense */}
+          <Script
+            strategy="afterInteractive"
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8339895143846620"
+            crossOrigin="anonymous"
+            id="adsbygoogle-script" // 添加了 id
+          ></Script>
+
+          {/* Google Analytics */}
+          <Script
+            strategy="afterInteractive"
+            src="https://www.googletagmanager.com/gtag/js?id=G-EQ27PZH0VM"
+            async
+            id="gtag-script" // 添加了 id
+          ></Script>
+          <Script
+            strategy="afterInteractive"
+            id="gtag-init-script" // 为内联脚本添加了 id
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-EQ27PZH0VM');
+              `,
+            }}
+          />
         </Head>
         <body>
           <Main />
